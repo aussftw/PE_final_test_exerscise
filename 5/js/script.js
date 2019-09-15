a238fba89aea4ab98db6a08c3948daae040ab2db
aussftw <shagronaus@gmail.com>
2019-09-15 14:56:51 +0300

    final commit

---
+++ 5/js/script.js
@@ -0,0 +1,13 @@
+const comment = document.getElementById("comment")
+const spamWord = document.getElementById("spam-word")
+const sendComment = document.getElementById("send-comment")
+const spamCheckResult = document.getElementById("spam-check-result")
+
+console.log(comment)
+console.log(spamWord)
+console.log(sendComment)
+console.log(spamCheckResult)
+
+function isSpam () {
+  counter = 0
+}
